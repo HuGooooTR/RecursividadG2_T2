aa2e1c2a7a0ee560d8481f1b4b418cc518abcb49
Hugoo <Hugoo@HUGO>
2026-09-09 14:21:30 -0600

    mensaje vrs2

---
+++ Recursividad.Logica/Recursividades.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursividad.Logica
+{
+    public class Recursividades
+    {
+        public int CalcularFactorial(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            //caso base o condicional
+            //caso general
+            return num * CalcularFactorial(num - 1);
+        }
+    }
+}
---
+++ Recursividad.UI.Consola/Program.cs
@@ -0,0 +1,9 @@
+﻿using Recursividad.Logica;
+
+Console.WriteLine("Métodos Recursivos");
+Console.WriteLine("\n\nCalcular Factorial de 5");
+
+
+Recursividades oRecursividades = new Recursividades();
+int resultado = oRecursividades.CalcularFactorial(5);
+Console.WriteLine(" Factorial de 5 es:"+resultado);
